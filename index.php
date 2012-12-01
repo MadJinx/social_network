9ce33ddf16cee3fa188548dc36c8e343dcb7c79c
Ryan Beverly <rbeverly@mymail.mines.edu>
2012-12-01 10:32:33 -0700

    added option to create new user to index

--- index.php
+++ index.php
@@ -15,13 +15,38 @@
 <body>
 	<h1>Welcome</h1>
 	<div>
+		<h3>Log In</h3>
 		<form action="user_check.php" method="post">
 			<fieldset>
-				<label for="username">Username:</label>
+				<label for="username">Username(email):</label>
 					<input type="text" name="username" id="username" />
 				<br />
 				<label for="password">Password:</label>
 					<input type="password" name="password" id="password" />
+				<br />
+				<input type="submit" value="Submit">
+			</fieldset>
+		</form>
+	</div>
+	<p>OR</p>
+	<div>
+		<h3>Create New User Profile</h3>
+		<form action="new_user.php" method="post">
+			<fieldset>
+				<label for="fname">First Name:</label>
+					<input type="text" name="fname" id="fname" />
+				<br />
+				<label for="lname">Last Name:</label>
+					<input type="text" name="lname" id="lname" />
+				<br />
+				<label for="email">Email Address:</label>
+					<input type="text" name="email" id="email" />
+				<br />
+				<label for="password">Password:</label>
+					<input type="password" name="password" id="password" />
+				<br />
+				<label for="repass">Re-Enter Password:</label>
+					<input type="password" name="repass" id="repass" />
 				<br />
 				<input type="submit" value="Submit">
 			</fieldset>
