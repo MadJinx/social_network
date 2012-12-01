15706eaab252ae6ace40beeb436adcb3a0079779
Craig Soto <csoto@mines.edu>
2012-12-01 12:44:46 -0700

    added edit link to profile and place for statuses

--- profile.php
+++ profile.php
@@ -1,3 +1,14 @@
+<!--
+Display the last 5 status updates made by the user that belongs to the profile
+Display the information of the user who the profile belongs to
+If the profile page belongs to the current user, then give them an option to edit the page
+Some logo or simple site description
+A menu that allows users to navigate to their profile or any other that they have access to on the site (home, help, about, contact, etc..) in addition to allowing them to logout
+Any additional features that fit on a social network's profile page are encouraged but not required 
+-->
+
+
+
 <?php
 	session_start();
 	
@@ -9,14 +20,26 @@
 	include_once("common.php");
 	// Call the common header function.
 	common_header();
+	
+	//need to set to the person's page we view. 
+	$_SESSION['profile'] = $_SESSION['user'];
 ?>
 <title>Your Profile</title>
 </head>
 <body>
+	<script type="text/javascript" src="profile_java.js"></script>
 	<h1>Welcome to your profile page</h1>
 	<div>
 		<p>You are logged in as <?= $_SESSION['user']."." ?></p>
 		<p><a href="logout.php">Logout?</a></p>
+		
+		<!-- need to change the second part of the condition -->
+		<?php if ( $_SESSION['user'] == $_SESSION['profile'] ){ ?>
+			<p><a href="edit.php">Edit</a></p>
+		<?php } ?>
+	</div>
+	<div>
+		<p> job, school, lives, relationship, birthday, likes, </p>
 	</div>
 	<div>
 		<p>Try uploading an image.</p>
@@ -31,5 +54,15 @@
 			</fieldset>
 		</form>
 	</div> 
+	<div>
+		<p> Status' Big Woophf want to fight about it </p>
+		<fieldset>
+			<p> Status 1 </p> </br>
+			<p> Status 2 </p> </br>
+			<p> Status 3 </p> </br>
+			<p> Status 4 </p> </br>
+			<p> Status 5 </p> </br>
+		</fieldset>
+	</div>
 </body>
 </html>
