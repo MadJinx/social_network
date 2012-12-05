f24f7a0ead4e01d425580cdf37c25828239f95b3
Joshua French <jfrench@mines.edu>
2012-12-04 21:35:51 -0700

    Josh
    - Added ability to add friends to friend list
    - Friend and Member lists display names on their individual pages

--- friends.php
+++ friends.php
@@ -9,14 +9,57 @@
 	include_once("common.php");
 	// Call the common header function.
 	common_header();
-	
-	//need to set to the person's page we view. 
-	$_SESSION['profile'] = $_SESSION['user'];
 ?>
 	<title>Your Friends</title>
 	</head>
 	<body>
 		<?php addMenu(); ?>
 		<h1>Welcome to the Friends Page</h1>
+
+		<?php
+			// Connect to DB
+			$db = new mysqli('localhost', 'team09', 'maroon', 'team09');
+			if (mysqli_connect_errno()) {
+				die('Failed to connect to database. Try again later.');
+			}
+
+			$query = 'select id from users where email = ?';
+			$prep_query = $db->prepare($query);
+			$prep_query->bind_param('s', $_SESSION['user']);
+			if ($prep_query->execute()) {
+				$prep_query->bind_result($id);
+				$prep_query->fetch();
+				$prep_query->close();
+			}
+			else {
+				die('Failed to execute query');
+			}
+			
+			$query = "select user2 from friends where user1 = $id";
+			$results = $db->query($query);
+
+			if (!$results) {
+				die('Invalid query');
+			}
+
+			$query = 'select fname, lname from users where id = ?';
+			$prep_query = $db->prepare($query);
+			while ($row = $results->fetch_assoc()) {
+				$id2 = $row['user2'];
+				$prep_query->bind_param('i', $id2);
+				if ($prep_query->execute()) {
+					$prep_query->bind_result($fname, $lname);
+					$prep_query->fetch();
+					echo "<p>$fname $lname</p>";
+				}
+				else {
+					die('Error obtaining friends list');
+				}
+			}
+
+			$prep_query->close();
+			$results->close();
+			$db->close();
+		?>
 	</body>
 </html>
--- members.php
+++ members.php
@@ -18,29 +18,34 @@
 	<body>
 		<?php addMenu(); ?>
 		<h1>Welcome to the Members Page</h1>
+		<h2>Befriend members by checking the checkbox next to their name and pressing the 'Save' button.</h2>
 
-		<?php
-			// Connect to DB
-			$db = new mysqli('localhost', 'team09', 'maroon', 'team09');
-			if (mysqli_connect_errno()) {
-				die('Failed to connect to database. Try again later.');
-			}
+		<form method="post" action="add_friends.php">
+			<?php
+				// Connect to DB
+				$db = new mysqli('localhost', 'team09', 'maroon', 'team09');
+				if (mysqli_connect_errno()) {
+					die('Failed to connect to database. Try again later.');
+				}
 
-			$query = 'select fname, lname from users';
-			$results = $db->query($query);
-			if (!$results) {
-				die('Invalid query' + mysqli_error());
-			}
+				$query = 'select fname, lname, id from users';
+				$results = $db->query($query);
+				if (!$results) {
+					die('Invalid query ' + mysqli_error());
+				}
 
-			while ($row = $results->fetch_assoc()) {
-				$fname = $row['fname'];
-				$lname = $row['lname'];
-				echo "<p>$fname $lname</p>";
-			}
+				while ($row = $results->fetch_assoc()) {
+					$id = $row['id'];
+					$fname = $row['fname'];
+					$lname = $row['lname'];
+					$checkbox = "<input type='checkbox' name='friends[]' value='$id'/>";
+					echo "<p>$fname $lname $checkbox</p>";
+				}
 
-			$results->close();
-			$db->close();
-		?>
-
+				$results->close();
+				$db->close();
+			?>
+			<input type='submit' value='Save'/>
+		</form>
 	</body>
 </html>
